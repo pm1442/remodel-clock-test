a0b95cc21d470a8b77ac26259570b8a406206328
pm1442 <support@palmtec.biz>
2026-09-03 11:49:10 -0400

    Add files via upload

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,7 @@
-const CACHE_NAME = 'ridgepoint-clock-v3';
+const CACHE_NAME = 'ridgepoint-clock-v4';
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/manifest.webmanifest', '/pwa-icon.png', '/pwa-icon-maskable.png'])));
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/', '/manifest.webmanifest', '/pwa-icon-2.png'])));
   self.skipWaiting();
 });
 
